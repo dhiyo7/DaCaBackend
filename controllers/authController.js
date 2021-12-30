5254ee4038293ad8e0630da163c944da6acff650
Dhiyo Reksa <dhiyodev7@gmail.com>
2021-12-30 16:01:58 +0700

    Auth Done

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,11 +2,9 @@ const {sellers, customers, Sequelize} = require("../models")
 const bcrypt = require('bcrypt')
 const jwt = require("jsonwebtoken")
 const BaseResponse = require("../helpers/response/response")
-const {error} = require("../helpers/response/response");
-const Op = Sequelize.Op
 
 module.exports = {
-    signupSeller : (req, res, next) => {
+    signupSeller: async (req, res, next) => {
         const {body} = req
         try {
             const saltRounds = 10
@@ -14,11 +12,12 @@ module.exports = {
             const createData = {
                 ...body,
             }
-            sellers.create(createData)
+            sellers
+                .create(createData)
                 .then((data) => {
                     BaseResponse.success(res, createData)
                 })
-                .catch((error) =>{
+                .catch((error) => {
                     delete createData.password
                     return BaseResponse.error(res, error)
                 })
@@ -27,5 +26,85 @@ module.exports = {
         }
     },
 
+    signInSeller: async (req, res, next) => {
+        const {body} = req
+        try {
+            let data = await sellers.findOne({
+                where: {email: body.email}
+            })
+            if (data === null) {
+                return BaseResponse.unauthorized(res, "User are not available on our records")
+            } else {
+                const payload = {
+                    id: data.id,
+                    name_shop: data.name_shop,
+                    email: data.email,
+                    photo: data.photo
+                }
+                const token = jwt.sign(payload, process.env.SECRET_TOKEN, {
+                    expiresIn: 31540000
+                })
+                delete data.dataValues.password
+                const newData = {
+                    ...data.dataValues,
+                    token
+                }
+                BaseResponse.success(res, newData)
+            }
+        } catch (e) {
+            BaseResponse.error(res, e)
+        }
+    },
 
+    signupCustomer: async (req, res, next) => {
+        const {body} = req
+        try {
+            const saltRounds = 10
+            body.password = bcrypt.hashSync(body.password, saltRounds)
+            const createData = {
+                ...body,
+            }
+            customers
+                .create(createData)
+                .then((data) => {
+                    BaseResponse.success(res, createData)
+                })
+                .catch((error) => {
+                    delete createData.password
+                    return BaseResponse.error(res, error)
+                })
+        } catch (e) {
+            return BaseResponse.error(res, e)
+        }
+    },
+
+    signInCustomer: async (req, res, next) => {
+        const {body} = req
+        try {
+            let data = await customers.findOne({
+                where: {email: body.email}
+            })
+            if (data === null) {
+                return BaseResponse.unauthorized(res, "User are not available on our records")
+            } else {
+                const payload = {
+                    id: data.id,
+                    name: data.name,
+                    email: data.email,
+                    photo: data.photo
+                }
+                const token = jwt.sign(payload, process.env.SECRET_TOKEN, {
+                    expiresIn: 31540000
+                })
+                delete data.dataValues.password
+                const newData = {
+                    ...data.dataValues,
+                    token
+                }
+                BaseResponse.success(res, newData)
+            }
+        } catch (e) {
+            BaseResponse.error(res, e)
+        }
+    },
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const authRouter = require('./auth')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* SignIn & SignUp */
+router.use('/auth', authRouter)
 
 module.exports = router;
